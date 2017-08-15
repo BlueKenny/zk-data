d3680bf5a061f467375935e85a45f89db8f76678
BlueKenny <bluez@linuxmail.org>
2017-08-15 13:53:09 +0200

    121

---
+++ Server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.6
+import sys
+import socket
+from debug import *
+
+SERVER_IP = ("127.0.0.1", 22222)
+s = socket.socket()
+
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(SERVER_IP)
+s.listen(5)
+
+while True:
+	Debug("Warte auf befehl...")
+	c, addr = s.accept()
+	Debug("Verbunden mit " + str(addr))
+	while True:
+		data = c.recv(2048)
+		if not data:
+			Debug("Client sendet nicht mehr")
+			break
+		data = data.decode()
+		Debug(str(data) + " erhalten")
+		Antwort = "Suche erfolgreich"
+		Antwort = Antwort.encode()
+		c.send(Antwort)
+c.close()
