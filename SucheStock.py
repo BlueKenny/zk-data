053f20291e7b7ece688bc9a013813c4c5cfd5c10
Zaunz Kenny <bluez@linuxmail.org>
2017-08-24 21:12:14 +0200

    Signed-off-by: Zaunz Kenny <bluez@linuxmail.org>

--- SucheStock.py
+++ SucheStock.py
@@ -9,44 +9,23 @@ from random import randint
 EntryList=["Bcode", "Artikel", "Name", "Ort", "PreisEK", "PreisVKH", "PreisVK", "Anzahl"]
 appSuche = gui("Stock Suche", "800x600") 
 
-def FuncSave(btn):
-	Debug("FuncSave")
-	for a in EntryList:
-		if not a == "Bcode":
-			BlueSave(a, appSuche.getEntry(a), datei)
+def tbFunc(btn):
+	Debug("btn : " + btn)
+
+tools = ["NEU", "LÖSCHEN"]
+appSuche.addToolbar(tools, tbFunc, findIcon=True)
+
+appSuche.addLabelEntry("Bcode")
+appSuche.addLabelEntry("Artikel")
+appSuche.addListBox("Suche")
+
+def Suche(btn):
+	Debug("Suche")
+
+#appSuche.bindKey("<Return>", FuncSuche)
+#appSuche.bindKey("<Escape>", FuncSave)
 
 
-def FuncSuche(btn):
-	global datei
-	Debug("FuncSuche")
-	BcodeSuche = appSuche.getEntry("Bcode")
-	Debug("BcodeSuche : " + str(BcodeSuche))
-	if len(BcodeSuche) == 6 and int(BcodeSuche):
-		pfad = "stock/" + BcodeSuche[-3] + BcodeSuche[-2] + BcodeSuche[-1] + "/"
-		Debug("pfad : " + str(pfad))
-		BlueMkDir(pfad)
-		datei = pfad + BcodeSuche
-		if os.path.exists(datei):
-			Debug("Laden")
-			for x in EntryList:
-				if not x == "Bcode":
-					appSuche.setEntry(x, BlueLoad(x, datei))
-		else:
-			Debug("Neu")
-			for x in EntryList:
-				if not x == "Bcode":
-					appSuche.setEntry(x, "")
-
-
-
-
-for EntryText in EntryList:
-	if EntryText == "Bcode":
-		appSuche.addLabelEntry(EntryText)
-		appSuche.addButton("Suche", FuncSuche)
-	else: 
-		appSuche.addLabelEntry(EntryText)
-
-appSuche.bindKey("<Return>", FuncSuche)
-appSuche.bindKey("<Escape>", FuncSave)
+appSuche.addLabel("info", "Enter = Suche")
+appSuche.bindKey("<Return>", Suche)
 appSuche.go()
