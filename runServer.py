37060eeb64be1bca08ac1527902dce5ab4e63baa
Zaunz Kenny <bluez@linuxmail.org>
2017-09-14 19:33:54 +0200

    Signed-off-by: Zaunz Kenny <bluez@linuxmail.org>

--- runServer.py
+++ runServer.py
@@ -6,6 +6,7 @@ from debug import *
 from BlueFunc import BlueMkDir, BlueLenDatei, BlueLoad, BlueSave
 import os
 while True:
-	os.system("python3 ./Server.py")
+	os.system("git pull origin master") 
+	time.sleep(1)
 	os.system("python3 ./Server.py")
 	time.sleep(5)
