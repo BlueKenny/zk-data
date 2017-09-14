f998a4596f8a5bbe7024caaffcb5a0abc5932dc7
Zaunz Kenny <bluez@linuxmail.org>
2017-09-14 20:08:10 +0200

    Signed-off-by: Zaunz Kenny <bluez@linuxmail.org>

--- RoundUp.py
+++ RoundUp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-def RoundUp05(Zahl): # 0.00
+def RoundUp05(Zahl): # 0.05
 	Zahl = float(Zahl) * 10.0000
 	ZahlRest = Zahl - int(Zahl)
 	if ZahlRest < 0.5: ZahlRest = 0.5000
@@ -8,12 +8,13 @@ def RoundUp05(Zahl): # 0.00
 	Zahl = int(Zahl) + ZahlRest
 	Zahl = float(Zahl) / 10.0000
 	return Zahl
-
-def RoundUp0005(Zahl): # 0.0000	
-	Zahl = float(Zahl) * 10.000000
-	ZahlRest = Zahl - int(Zahl)
-	if ZahlRest < 0.5: ZahlRest = 0.500000
-	else: ZahlRest = 1.000000
-	Zahl = int(Zahl) + ZahlRest
-	Zahl = float(Zahl) / 10.0000
+def RoundUp0000(Zahl): # 0.0000
+	Zahl = float(Zahl) * 10000.0000
+	Zahl = int(Zahl)
+	Zahl = float(Zahl) / 10000.0000
+	return Zahl
+def RoundUp00(Zahl): # 0.00
+	Zahl = float(Zahl) * 100.0000
+	Zahl = int(Zahl)
+	Zahl = float(Zahl) / 100.0000
 	return Zahl
