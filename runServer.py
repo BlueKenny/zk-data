3a7e6cc0e44a934a45b3f2b395ec29cc4ea33373
Zaunz Kenny <bluez@linuxmail.org>
2017-09-14 19:31:41 +0200

    Signed-off-by: Zaunz Kenny <bluez@linuxmail.org>

--- runServer.py
+++ runServer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import sys
 import socket
+import time
 from debug import *
 from BlueFunc import BlueMkDir, BlueLenDatei, BlueLoad, BlueSave
 import os
 while True:
-	os.system("python3 ./update")
 	os.system("python3 ./Server.py")
+	os.system("python3 ./Server.py")
+	time.sleep(5)
