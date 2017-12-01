bd83fa5b34ed4c867fb111daff662608a112da12
BlueKenny <bluez@linuxmail.org>
2017-12-01 20:15:57 +0100

    Signed-off-by: BlueKenny <bluez@linuxmail.org>

--- runServer.py
+++ runServer.py
@@ -5,8 +5,16 @@ import time
 from debug import * 
 from BlueFunc import BlueMkDir, BlueLenDatei, BlueLoad, BlueSave
 import os
+try: 
+	import urllib.request
+	MakeUpdate = True
+except: MakeUpdate = False
+Debug("MakeUpdate : " + str(MakeUpdate))
 while True:
-	os.system("git pull origin master") 
+	#os.system("git pull origin master") 
+	if MakeUpdate:
+		Debug("Update Startet")
+		urllib.request.urlretrieve("https://raw.githubusercontent.com/BlueKenny/zk-data/master/runServer.py", "file.py")
 	time.sleep(1)
 	os.system("python3 ./Server.py")
 	time.sleep(3)
