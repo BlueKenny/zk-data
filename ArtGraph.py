08132aa985d6c8dff06b980e6b03ff6e0e002a55
BlueKenny <bluez@linuxmail.org>
2018-01-19 14:54:44 +0100

    ArtGraph as App
    ArtGraph is no more a library

---
+++ ArtGraph.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from libs.appjar0830 import gui 
+#from numpy import sin, pi, arange
+import random
+from libs.send import *
+import sys
+
+if len(sys.argv) == 2:
+	ID = sys.argv[1]
+
+def Datum_Anzahl(ID):
+
+	DATA=StockGetBewegung(ID)
+	DATA=DATA.replace("\']", "").replace("[\'", "")
+	DATA=DATA.split("\', \'")
+	print(DATA)
+	x=[]
+	y=[]
+	for vars in DATA:
+		x.append(vars.split("|")[0])
+		y.append(vars.split("|")[1])
+
+	def generate(btn):
+	    # *getXY() will unpack the two return values
+	    # and pass them as separate parameters
+	    app.updatePlot("p1", *getXY())
+	    showLabels()
+
+	def showLabels():
+	    axes.legend(['Stock von ' + str(ID)])
+	    axes.set_xlabel("Monate")
+	    axes.set_ylabel("Anzahl")
+	    app.refreshPlot("p1")
+
+	app = gui(handleArgs=False)
+	#app.addLabel("ID", "123456")
+	axes = app.addPlot("p1", x, y)
+	showLabels()
+	#app.addButton("Generate", generate)
+	app.go()
+
+Datum_Anzahl(ID)
