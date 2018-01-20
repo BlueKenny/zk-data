3d0d7d9b04bc22e045841e35ddc56e7f532f9474
BlueKenny <bluez@linuxmail.org>
2018-01-20 07:25:36 +0100

    initial work for invoice checking

--- CheckInvoice.py
+++ CheckInvoice.py
@@ -5,3 +5,10 @@ import getpass, poplib
 Mailbox = poplib.POP3_SSL('', '995') 
 Mailbox.user("") 
 Mailbox.pass_("") 
+# detach.py -H imap.example.com -u user -p password  -f ~/tmp/attachments/{year}/{from}/{name} -v 'attached'
+# detach.py -H imap.example.com -u user -p password  -f ~/tmp/attachments/{year}/{from}/{name.subject_section}&nbsp; -v --threads 5 'attached'
+
+
+# http://zderadicka.eu/download-email-attachments-automagically/
+
+# pip3 install -e https://github.com/izderadicka/imap_detach
