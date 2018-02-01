107ee6bb33cf60905aaa8a136612213e5e7e4531
Zaunz Kenny <bluez@linuxmail.org>
2018-02-01 13:54:42 +0100

    fix

--- libs/barcode.py
+++ libs/barcode.py
@@ -109,7 +109,7 @@ def PrintBarcode(IP, ID, Barcode, Name, Price):
     else:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((TCP_IP, TCP_PORT))
-        s.send(bytes(zpl, "utf-8"))
+        s.send(bytes(zpl, "utf-16"))
         s.close()
 
 
@@ -133,7 +133,7 @@ def PrintLocation(Location):
     else:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((TCP_IP, TCP_PORT))
-        s.send(bytes(zpl, "utf-8"))
+        s.send(bytes(zpl, "utf-16"))
         s.close()
 
 
