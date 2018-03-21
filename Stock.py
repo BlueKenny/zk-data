8857f20a544cb49f4ad018cc2f73d05152cd087d
Kenny <kenny@ktux.lan>
2018-03-21 13:58:13 +0100

    f

--- Stock.py
+++ Stock.py
@@ -13,35 +13,8 @@ os.system("export MIR_SOCKET=/var/run/mir_socket")
 class Main:    
     def __init__(self):
         print("init")
-        #for user in os.listdir("/home/"):
-        #    print(user)
-        User = os.popen("echo $USER").readlines()[0].rstrip()
-        
-        if os.path.exists("/home/phablet"): Desktop = "/home/phablet/.local/share/applications"
-        else: Desktop = os.popen("echo $(xdg-user-dir DESKTOP)").readlines()[0].rstrip()
-        
-        file = Desktop + "/Stock.desktop"
-        #os.system("rm " + file)
-        if not os.path.exists(file):
-            print("Write Desktop Entry")
-            print("User: " + str(User))
-            print("Desktop: " + str(Desktop))
-            print("file: " + str(file))
-            DesktopEntry = open(file, "a")
-            DesktopEntry.write("[Desktop Entry]\n")
-            DesktopEntry.write("Name=Stock\n")
-            DesktopEntry.write("Path=/home/" + User + "/zk-data/\n")
-            DesktopEntry.write("Exec=qmlscene /home/" + User + "/zk-data/Stock.qml\n")
-            DesktopEntry.write("Terminal=false\n")
-            DesktopEntry.write("X-Ubuntu-Touch=true\n")
-            DesktopEntry.write("Type=Application\n")
-            DesktopEntry.write("StartupNotify=true\n")
-            DesktopEntry.write("Icon=None\n")
-             
-            os.system("chmod +x " + file)
-            
-        os.system("git pull")
         self.busy(False)
+        self.ScanForSearch()
     
     def ScanForSearch(self):
         self.busy(True)
