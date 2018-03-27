161c784b9d0629d34ee91441f2ea5ea2f4e3cfba
Kenny <kenny@ktux.lan>
2018-03-27 17:37:10 +0200

    f

--- qml/libs/BlueFunc.py
+++ qml/libs/BlueFunc.py
@@ -5,71 +5,86 @@ import time
 import datetime
 import codecs
 
+def forPhone(file):
+    if os.path.exists("/home/phablet"):
+        if not "/home/phablet/.local/bluekenny.zk-data/" in file:
+            file = "/home/phablet/.local/bluekenny.zk-data/" + file
+            file = file.replace("//", "/")
+    return str(file)
+
 def Date():
-	now = datetime.datetime.now()
-	zeit = str(now.strftime("%Y-%m-%d"))
-	return zeit
+    now = datetime.datetime.now()
+    zeit = str(now.strftime("%Y-%m-%d"))
+    return zeit
 
 def Timestamp():
-	return time.time()
+    return time.time()
 
 def BlueLenDatei(File):	#	Gibt die anzahl linie zuruck in einem dokument
-	if os.path.exists(File): 
-		Datei = open(File, "r")
-		DateiDatenIndex = Datei.readlines()
-		Datei.close()
-		return len(DateiDatenIndex)
+    File = forPhone(File)
+
+    if os.path.exists(File): 
+        Datei = open(File, "r")
+        DateiDatenIndex = Datei.readlines()
+        Datei.close()
+        return len(DateiDatenIndex)
 
 def BlueMkDir(directory):#	Macht ein verzeichnis wenn es noch nicht existiert
-	if not os.path.exists(directory):
-		os.makedirs(directory)
-		#os.mkdir(directory)
+    directory = forPhone(directory)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        #os.mkdir(directory)
 
 
 
 
 SplitIt = "&zKz&"
 def BlueLoad(VarName, File):
-	if os.path.exists(File): 
-		try:
-			Datei = codecs.open(File, "r", "utf-8")
-			DateiDatenIndex = Datei.readlines()
-			Datei.close()
-		except:
-			Datei = codecs.open(File, "r", "latin-1")
-			DateiDatenIndex = Datei.readlines()
-			Datei.close()
-		Gefunden=False
-		for AlleLinien in DateiDatenIndex:
-			LinienVarName = AlleLinien.split(SplitIt)[0]
-			if VarName == LinienVarName:	
-				SavedData = AlleLinien.split(SplitIt)[1].rstrip()
-				return SavedData
+    File = forPhone(File)
+
+    if os.path.exists(File): 
+        try:
+            Datei = codecs.open(File, "r", "utf-8")
+            DateiDatenIndex = Datei.readlines()
+            Datei.close()
+        except:
+            Datei = codecs.open(File, "r", "latin-1")
+            DateiDatenIndex = Datei.readlines()
+            Datei.close()
+        Gefunden=False
+        for AlleLinien in DateiDatenIndex:
+            LinienVarName = AlleLinien.split(SplitIt)[0]
+            if VarName == LinienVarName:	
+                SavedData = AlleLinien.split(SplitIt)[1].rstrip()
+                return SavedData
 
 def BlueSave(VarName, VarData, File):
-	if os.path.exists(File): 
-		Datei = open(File, "r", errors="ignore")
-		DateiDatenIndex = Datei.readlines()
-		Datei.close()
-		Gefunden=False
-		for AlleLinien in DateiDatenIndex:
-			LinienVarName = AlleLinien.split(SplitIt)[0]
-			if VarName == LinienVarName:	
-				Gefunden=True
-				LinienVarData = AlleLinien.split(SplitIt)[1]
+    File = forPhone(File)
 
-				Datei = open(File, "r", errors="ignore")
-				DateiDaten = Datei.read()
-				Datei.close()
+    if os.path.exists(File): 
+        Datei = open(File, "r", errors="ignore")
+        DateiDatenIndex = Datei.readlines()
+        Datei.close()
+        Gefunden=False
+        for AlleLinien in DateiDatenIndex:
+            LinienVarName = AlleLinien.split(SplitIt)[0]
+            if VarName == LinienVarName:	
+                Gefunden=True
+                LinienVarData = AlleLinien.split(SplitIt)[1]
 
-				Datei = open(File, "w")
-				Datei.write(DateiDaten.replace(str(LinienVarName) + str(SplitIt) + str(LinienVarData), str(VarName) + str(SplitIt) + str(VarData) + "\n"))
-				Datei.close()
-		if not Gefunden:
-			Datei = open(File, "a")
-			Datei.write("\n" + str(VarName) + str(SplitIt) + str(VarData))
-			Datei.close()
-	else:
-		Datei = open(File, "w")
-		Datei.write(str(VarName) + str(SplitIt) + str(VarData))
+                Datei = open(File, "r", errors="ignore")
+                DateiDaten = Datei.read()
+                Datei.close()
+
+                Datei = open(File, "w")
+                Datei.write(DateiDaten.replace(str(LinienVarName) + str(SplitIt) + str(LinienVarData), str(VarName) + str(SplitIt) + str(VarData) + "\n"))
+                Datei.close()
+        if not Gefunden:
+            Datei = open(File, "a")
+            Datei.write("\n" + str(VarName) + str(SplitIt) + str(VarData))
+            Datei.close()
+    else:
+        Datei = open(File, "w")
+        Datei.write(str(VarName) + str(SplitIt) + str(VarData))
 
